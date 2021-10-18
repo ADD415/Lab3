cd81157bdd39b6714b7f5e6228287caabb9f91bd
ADD415 <ADAVID212@caledonian.ac.uk>
2021-10-18 19:53:13 +0100

    Lab 3

---
+++ Assets/Scripts/Gun.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    public Camera cam;
+    public float force = 1f;
+    public GameObject[] bulletHolePrefabs;
+
+    //FireRate
+    public bool canShoot;
+    public float fireRateTimer = 0f;
+    public float fireRateTimerMax = 5f;
+
+    //Reload
+    public int currentAmmo;
+    public int ammoMax = 10;
+    public bool reloading = false;
+    public float reloadTimer;
+    public float reloadTimerMax = 10f;
+    private bool pressedRecently;
+
+
+        void Start()
+        {
+          currentAmmo = ammoMax;
+        }
+
+
+        void Update()
+        {
+            
+            if (currentAmmo <= 0f)
+            {
+                reloading = true;
+                reloadTimer += Time.deltaTime;
+                
+                if (reloadTimer > reloadTimerMax)
+                {
+                    currentAmmo = ammoMax;
+                    reloading = false;
+                    reloadTimer = 0f;
+                    pressedRecently = false;
+                }
+        
+            }
+
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                reloading = true;
+                pressedRecently = true;
+            }
+            
+            
+            if (canShoot == false)
+            {
+               fireRateTimer += Time.deltaTime; 
+               
+               if (fireRateTimer > fireRateTimerMax)
+               {
+                    canShoot = true;
+                    fireRateTimer = 0; 
+               }
+            }
+
+            
+
+            if (Input.GetKeyDown(KeyCode.Mouse0) && canShoot == true )
+            {
+               Shoot();
+            }
+
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Fire();
+        }
+
+    }
+
+
+    public void Fire()
+    {
+        Ray ray = cam.ViewportPointToRay(new Vector3(0.5F, 0.5F, 0));
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit) && Time.timeScale == 1)
+        {
+
+            GameObject choosenBulletHole = bulletHolePrefabs[Random.Range(0, bulletHolePrefabs.Length)];
+
+            if (hit.rigidbody && Time.timeScale == 0)
+            {
+                var direction = new Vector3(hit.transform.position.x - transform.position.x, hit.transform.position.y - transform.position.y, hit.transform.position.z - transform.position.z);
+                hit.rigidbody.AddForceAtPosition(force * Vector3.Normalize(direction), hit.point);
+
+                var tempBullet = Instantiate(choosenBulletHole, hit.point, Quaternion.LookRotation(hit.normal));
+                tempBullet.transform.parent = hit.transform;
+            }
+            else
+            {
+                Instantiate(choosenBulletHole, hit.point, Quaternion.LookRotation(hit.normal));
+            }
+        }
+        else
+            print("I'm looking at nothing!");
+
+    }
+
+
+    void Shoot()
+        {  
+            currentAmmo--;
+            canShoot = false;
+        }
+
+}
---
+++ Assets/Scripts/Manager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Manager : MonoBehaviour
+{
+    public bool paused;
+    public Slider fovSlider;
+    public Camera cam;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        fovSlider.gameObject.SetActive(false);
+        paused = false;
+        cam.fieldOfView = fovSlider.value;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P) && !paused)
+        {
+            Time.timeScale = 0;
+            paused = !paused;
+
+            Cursor.lockState = CursorLockMode.None;
+            fovSlider.gameObject.SetActive(true);
+            return;
+        }
+
+        if (Input.GetKeyDown(KeyCode.P) && paused)
+        {
+            Time.timeScale = 1;
+            paused = !paused;
+
+            Cursor.lockState = CursorLockMode.Locked;
+            fovSlider.gameObject.SetActive(false);
+        }
+
+        cam.fieldOfView = fovSlider.value;
+
+    }
+}
